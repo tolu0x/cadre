1c3c5984504d6b18aa8f2b9e288e2d657b243b88
Toluwalase Adams <theonlytolu@gmail.com>
2026-02-26 16:30:19 +0100

    add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { IBM_Plex_Mono, Bebas_Neue } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
 
 const ibmPlexMono = IBM_Plex_Mono({
   variable: "--font-ibm-plex-mono",
@@ -26,6 +27,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${ibmPlexMono.variable} ${bebasNeue.variable} antialiased`}>
         {children}
+        <Analytics />
       </body>
     </html>
   );
